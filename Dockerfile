cafac587aecb9d7b0dc697f107e879be33e5fc2f
Lucas Godoy <lucas.godoy@rappi.com>
2020-08-05 15:08:07 -0300

    testing env vars

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,3 @@
-FROM golang:1.13.4  as builder
-WORKDIR /go/src/bitbucket.org/rappinc/cpgs-int-worker
-COPY go.mod .
-COPY go.sum .
-RUN go mod download
-COPY . .
-RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
-
-FROM alpine
-RUN apk --no-cache add ca-certificates
-COPY --from=builder /go/src/bitbucket.org/rappinc/cpgs-int-worker/app .
-CMD ["./app"]
+FROM python:3.9.0b5-alpine3.12 as builder
+WORKDIR /tmp/builder
+COPY ./krakend .
--- krakend/env_var_replacer.py
+++ krakend/env_var_replacer.py
@@ -1,6 +1,28 @@
-import re
+from tempfile import mkstemp
+from shutil import move, copymode
+from os import fdopen
+import re, os
 
-with open('../krakend/krakend.json', 'r') as config:
-    for line in config:
-        x = re.search("\{\s\$.*\s\}", line)
-        print(x)
+regex = "\{\s(\$.*?)\s\}"
+
+fh, abs_path = mkstemp()
+with fdopen(fh,'w') as new_file:
+    with open('./krakend.json') as old_file:
+        for line in old_file:
+            exists = re.search(regex, line)
+            if not exists:
+                new_file.write(line)
+                continue
+
+            pattern = exists.group()
+            env_var_name = pattern[3:len(pattern)-2]
+            env_var_value = os.getenv(env_var_name)
+
+            if env_var_value is None:
+                raise Exception(f"the {env_var_name} secret is not set")
+
+            new_file.write(line.replace(pattern, env_var_value))
+
+    copymode('krakend.json', abs_path)
+    move(abs_path, './replaced_with_env_values.json')
+
