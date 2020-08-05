a5424a46be17e383eeccdec43a21b45bd57f5157
Lucas Godoy <lucas.godoy@rappi.com>
2020-08-05 17:10:28 -0300

    Replace all env var names within json config

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,11 @@
-FROM python:3.9.0b5-alpine3.12 as builder
-WORKDIR /tmp/builder
-COPY ./krakend .
+FROM devopsfaith/krakend:1.1.1
+USER root
+RUN apt-get update -qq && \
+    apt-get install python3 -qq > /dev/null
+
+COPY ./krakend /etc/krakend
+COPY ./docker-entrypoint.sh .
+
+# Overrides krakend's parent image entrypoint
+ENTRYPOINT []
+CMD sh docker-entrypoint.sh
--- krakend/env_var_replacer.py
+++ krakend/env_var_replacer.py
@@ -1,12 +1,11 @@
-from tempfile import mkstemp
+# from tempfile import mkstemp
 from shutil import move, copymode
 from os import fdopen
 import re, os
 
 regex = "\{\s(\$.*?)\s\}"
 
-fh, abs_path = mkstemp()
-with fdopen(fh,'w') as new_file:
+with open('./krakend-final.json','w+') as new_file:
     with open('./krakend.json') as old_file:
         for line in old_file:
             exists = re.search(regex, line)
@@ -23,6 +22,26 @@ with fdopen(fh,'w') as new_file:
 
             new_file.write(line.replace(pattern, env_var_value))
 
-    copymode('krakend.json', abs_path)
-    move(abs_path, './replaced_with_env_values.json')
+    copymode('krakend.json', 'krakend-final.json')
+#     move(abs_path, './krakend-final.json')
 
+# fh, abs_path = mkstemp()
+# with fdopen(fh,'w') as new_file:
+#     with open('./krakend.json') as old_file:
+#         for line in old_file:
+#             exists = re.search(regex, line)
+#             if not exists:
+#                 new_file.write(line)
+#                 continue
+#
+#             pattern = exists.group()
+#             env_var_name = pattern[3:len(pattern)-2]
+#             env_var_value = os.getenv(env_var_name)
+#
+#             if env_var_value is None:
+#                 raise Exception(f"the {env_var_name} secret is not set")
+#
+#             new_file.write(line.replace(pattern, env_var_value))
+#
+#     copymode('krakend.json', abs_path)
+#     move(abs_path, './krakend-final.json')
